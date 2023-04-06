460458adcfa9b1e901ef7cd8d9b46becb8886dc3
jack-skinner-552 <jack.skinner.552@gmail.com>
2023-04-06 01:23:57 -0600

    Updating PokeAPI programs

--- Python/PokeAPI/Pokemon_Type_List_Graphic.py
+++ Python/PokeAPI/Pokemon_Type_List_Graphic.py
@@ -1,76 +1,91 @@
-# This program gets a Pokemon Type from user input, then creates an image listing all the Pokemon that belongs to that type up to Generation VIII
+# Pokemon_Type_List_Graphic.py
 
+# This program gets a Pokemon Type from user input, then creates an image
+# listing all the Pokemon that belongs to that type up to Generation VIII
 import requests
 from tqdm import tqdm
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 
-# Get the Pokemon Type from the user
-p_type = input('Enter a Pokemon Type: ')
 
-# The PokeAPI URL for the Pokemon Type
-type_url = 'https://pokeapi.co/api/v2/type/' + p_type.lower()
+def get_pokemon_list(p_type):
+    # The PokeAPI URL for the Pokemon Type
+    type_url = 'https://pokeapi.co/api/v2/type/' + p_type.lower()
 
-# Make a GET request to the PokeAPI endpoint that returns information about input type Pokemon
-response = requests.get(type_url)
+    # Make a GET request to the PokeAPI endpoint that returns information about input type Pokemon
+    response = requests.get(type_url)
 
-# If the request was successful, extract the JSON data from the response
-if response.status_code == 200:
-    data = response.json()
+    # If the request was successful, extract the JSON data from the response
+    if response.status_code == 200:
+        data = response.json()
 
+        pokemon_urls = [pokemon['pokemon']['url'] for pokemon in data['pokemon'] if int(pokemon['pokemon']['url'].split('/')[-2]) <= 905]
+        pokemon_data = [requests.get(url).json() for url in tqdm(pokemon_urls, desc="Fetching Pokemon Data")]
+
+        type_pokemon = []
+        for pokemon in pokemon_data:
+            if any(type_info['type']['name'] == p_type.lower() for type_info in pokemon['types']):
+                pokemon_id = pokemon['id']
+                # Generation 9 Pokemon ID information in PokeAPI is currently incorrect, for now, only include Generations 1-8
+                if pokemon_id <= 905:
+                    pokemon_name = pokemon['name']
+                    types = [t["type"]["name"] for t in pokemon["types"]]
+                    types = [t.capitalize() for t in types]
+                    type_pokemon.append({'id': pokemon_id, 'name': pokemon_name.capitalize(), 'types': types})
+
+        n_pokemon = len(type_pokemon)
+        print(f'There are {n_pokemon} {p_type} type Pokemon.')
+        for pokemon in type_pokemon:
+            print(f"ID: {pokemon['id']} | Name: {pokemon['name']} | Types: {', '.join(pokemon['types'])}")
+
+        return type_pokemon
+    else:
+        print("Failed to get Pokemon list.")
+        return None
+
+
+def create_pokemon_image(type_pokemon, p_type):
     # Create a blank image with a white background
     width = 800
-    height = 96 * len(data['pokemon'])
+    height = 96 * len(type_pokemon)
     img = Image.new("RGB", (width, height), "white")
 
     # Set the font for the Pokemon names and types
     name_font = ImageFont.truetype("arial.ttf", 20)
     type_font = ImageFont.truetype("arial.ttf", 16)
 
-    # Filter the results to only include default input type Pokemon & Add them to Image
+    # Add the Pokemon sprites and information to the image
     x = 0
     y = 0
-    type_pokemon = []
-    for pokemon in tqdm(data['pokemon']):
-        pokemon_url = pokemon['pokemon']['url']
-        pokemon_data = requests.get(pokemon_url).json()
-        if pokemon_data['is_default'] and any(type_info['type']['name'] == p_type.lower() for type_info in pokemon_data['types']):
-            pokemon_id = pokemon_data['id']
-            # Generation IX Pokemon ID information in PokeAPI is currently incorrect, for now, only include Generations I-VIII
-            if pokemon_id <= 905:
-                pokemon_name = pokemon_data['name'].capitalize()
-                sprite_url = pokemon_data["sprites"]["other"]["official-artwork"]["front_default"]
-                sprite_response = requests.get(sprite_url)
-                sprite_data = sprite_response.content
-                sprite_img = Image.open(BytesIO(sprite_data))
-                sprite_img = sprite_img.resize((96, 96))
-                img.paste(sprite_img, (x, y))
-                draw = ImageDraw.Draw(img)
-                types = [t["type"]["name"].capitalize() for t in pokemon_data["types"]]
-                types_str = "/".join(types)
-                id_text = f"#{pokemon_id}"
-                name_text = f"{pokemon_name} ({types_str})"
-                id_bbox = draw.textbbox((x + 2, y + 36), id_text, font=type_font)
-                id_width = id_bbox[2] - id_bbox[0]
-                draw.text((x + 48 - id_width / 2 + 200, y + 36), id_text, font=type_font, fill="black")
-                name_bbox = draw.textbbox((x + 48, y + 36), name_text, font=name_font)
-                name_width = name_bbox[2] - name_bbox[0]
-                draw.text((x + 48 - name_width / 2 + 480, y + 36), name_text, font=name_font, fill="black")
-                draw.line((0,y,width,y), fill="black")
-                y += 96
-                type_pokemon.append({'id': pokemon_id, 'name': pokemon_name, 'types': types})
+    for pokemon in tqdm(type_pokemon, desc="Creating List Image"):
+        sprite_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{pokemon['id']}.png"
+        sprite_response = requests.get(sprite_url)
+        sprite_data = sprite_response.content
+        sprite_img = Image.open(BytesIO(sprite_data))
+        sprite_img = sprite_img.resize((96, 96))
+        img.paste(sprite_img, (x, y))
+        draw = ImageDraw.Draw(img)
+        types_str = "/".join(pokemon['types'])
+        id_text = f"#{pokemon['id']}"
+        name_text = f"{pokemon['name']} ({types_str})"
+        id_bbox = draw.textbbox((x + 2, y + 36), id_text, font=type_font)
+        id_width = id_bbox[2] - id_bbox[0]
+        draw.text((x + 48 - id_width / 2 + 200, y + 36), id_text, font=type_font, fill="black")
+        name_bbox = draw.textbbox((x + 48, y + 36), name_text, font=name_font)
+        name_width = name_bbox[2] - name_bbox[0]
+        draw.text((x + 48 - name_width / 2 + 480, y + 36), name_text, font=name_font, fill="black")
+        draw.line((0, y, width, y), fill="black")
+        y += 96
 
-        n_pokemon = len(type_pokemon)
+    img.save("pokemon_list_" + p_type.lower() + ".png")
 
-        # Any unused space in the image is cropped out, and the final image is saved
-        new_height = n_pokemon * 96
-        new_img = img.crop((0, 0, width, new_height))
-        new_img.save("pokemon_list_" + p_type.lower() +".png")
 
-    # Print the list of input type Pokemon
-    print(f'There are {n_pokemon} {p_type} type Pokemon.')
-    for pokemon in type_pokemon:
-        print(f"ID: {pokemon['id']} | Name: {pokemon['name']} | Types: {', '.join(pokemon['types'])}")
+def main():
+    # Get the Pokemon Type from the user
+    p_type = "Dragon"
 
-else:
-    print("Failed to get Pokemon list.")
+    create_pokemon_image(get_pokemon_list(p_type), p_type)
+
+
+if __name__ == "__main__":
+    main()
--- Python/PokeAPI/Pokemon_weak_against_move_type.py
+++ Python/PokeAPI/Pokemon_weak_against_move_type.py
@@ -1,72 +1,130 @@
-# Program asks for a Pokemon Type and displays the pokemon who are weak aganst given type moves.
+# Program asks for a Pokemon Type and displays the pokemon who are weak against given type moves.
 
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 from tqdm import tqdm
 import requests
+import os
 
-# Get the move type from the user
-move_type = input("Enter a move type: ")
 
-# The PokeAPI URL for the move type
-move_type_url = "https://pokeapi.co/api/v2/type/" + move_type.lower()
+def get_pokemon_list(m_type):
+    # The PokeAPI URL for the move type
+    move_type_url = "https://pokeapi.co/api/v2/type/" + m_type.lower()
 
-# Make an API request to retrieve information about the move type
-response = requests.get(move_type_url)
+    # Make an API request to retrieve information about the move type
+    response = requests.get(move_type_url)
 
-# If the request was successful (status code of 200)
-if response.status_code == 200:
-    # Extract the lists of Pokemon types that are weak against, strong against, and immune to input type moves
-    type_data = response.json()
-    type_strengths = type_data["damage_relations"]["double_damage_to"]
-    type_strengths_names = [strength["name"] for strength in type_strengths]
-    type_weaknesses = type_data["damage_relations"]["half_damage_to"]
-    type_weakness_names = [weakness["name"] for weakness in type_weaknesses]
-    type_no_damage = type_data["damage_relations"]["no_damage_to"]
-    type_no_damage_names = [no_damage["name"] for no_damage in type_no_damage]
+    # If the request was successful (status code of 200)
+    if response.status_code == 200:
+        # Extract the lists of Pokemon types that are weak against, strong against, and immune to input type moves
+        type_data = response.json()
+        type_strengths = type_data["damage_relations"]["double_damage_to"]
+        type_strengths_names = [strength["name"] for strength in type_strengths]
+        type_weaknesses = type_data["damage_relations"]["half_damage_to"]
+        type_weakness_names = [weakness["name"] for weakness in type_weaknesses]
+        type_no_damage = type_data["damage_relations"]["no_damage_to"]
+        type_no_damage_names = [no_damage["name"] for no_damage in type_no_damage]
 
-    # Print the Types the Input is strong against, weak against, and immune to
-    if type_strengths_names:
-        print(move_type, "is strong against", type_strengths_names)
-    print(move_type, "is weak against", type_weakness_names)
-    if type_no_damage_names:
-        print(move_type, "does no damage to", type_no_damage_names)
+        # Print the Types the Input is strong against, weak against, and immune to
+        if type_strengths_names:
+            print(m_type, "is strong against", type_strengths_names)
+        print(m_type, "is weak against", type_weakness_names)
+        if type_no_damage_names:
+            print(m_type, "does no damage to", type_no_damage_names)
 
-    # If "Normal" is the Input, print one line and end program    
-    if move_type.lower() == "normal":
-        print("There are no Pokemon who are weak to Normal moves.")
-    else:
-
-        # Make an API request to retrieve information about all Pokemon
-        all_pokemon_url = "https://pokeapi.co/api/v2/pokemon?limit=1118"
-        response = requests.get(all_pokemon_url)
-
-        # If the request was successful (status code of 200)
-        if response.status_code == 200:
-            # Extract the list of all Pokemon
-            all_pokemon_data = response.json()
-            all_pokemon_list = all_pokemon_data["results"]
-
-            # Make a list of Pokemon that are weak to input type moves
+        # If "Normal" is the Input, print one line and end program
+        if m_type.lower() == "normal":
+            print("There are no Pokémon who are weak to Normal moves.")
+            return []
+        else:
             type_weak_pokemon = []
-            for pokemon in tqdm(all_pokemon_list):
-                pokemon_url = pokemon["url"]
-                pokemon_response = requests.get(pokemon_url)
-                pokemon_data = pokemon_response.json()
-                pokemon_id = pokemon_data["id"]
-                pokemon_name = pokemon["name"].capitalize()
-                pokemon_types = [type["type"]["name"] for type in pokemon_data["types"]]
-                if set(type_strengths_names) & set(pokemon_types):
-                    if not set(type_weakness_names) & set(pokemon_types):
-                        if not set(type_no_damage_names) & set(pokemon_types):
-                            type_weak_pokemon.append((pokemon_id, pokemon_name, pokemon_types))
-    
-            # Sort the list of weak Pokemon by ID number
-            type_weak_pokemon.sort()
+            # Make an API request to retrieve information about all Pokemon types
+            for pokemon_type in tqdm(type_strengths_names, desc="Fetching Pokemon Data"):
+                url = f"https://pokeapi.co/api/v2/type/{pokemon_type}"
+                response = requests.get(url)
+
+                if response.status_code == 200:
+                    data = response.json()
+                    pokemon_urls = [p['pokemon']['url'] for p in data['pokemon'] if int(p['pokemon']['url'].split('/')[-2]) <= 905]
+                    pokemon_data = [requests.get(u).json() for u in pokemon_urls]
+                    for p in pokemon_data:
+                        try:
+                            name = p["name"].capitalize()
+                            id = p["id"]
+                            types = [t["type"]["name"] for t in p["types"]]
+                            if not set(type_weakness_names) & set(types):
+                                if not set(type_no_damage_names) & set(types):
+                                    if {'id': id, 'name': name, 'types': types} not in type_weak_pokemon:
+                                        type_weak_pokemon.append({'id': id, 'name': name, 'types': types})
+                        except KeyError:
+                            continue
+
+            # Sort the list of weak Pokemon by ID
+            type_weak_pokemon.sort(key=lambda x: int(x["id"]))
 
             # Print the names of the weak Pokemon, their ID numbers, and their types
-            print(f"Pokemon weak to {move_type} moves:")
+            print(f"Pokémon weak to {m_type} moves:")
             for pokemon in type_weak_pokemon:
-                print(f'#: {pokemon[0]} | {pokemon[1]} | Types: {pokemon[2]}')
-        else:
-            print("Error retrieving information about all Pokemon from PokeAPI")
-else:
-    print("Error retrieving information about the move type from PokeAPI")
+                print(f"ID: {pokemon['id']} | Name: {pokemon['name']} | Types: {', '.join(pokemon['types'])}")
+
+            return type_weak_pokemon
+
+    else:
+        print("Error retrieving information about the move type from PokeAPI")
+        return []
+
+
+def create_pokemon_image(pokemon_list, m_type):
+    # Create a blank image with a white background
+    width = 800
+    height = 96 * len(pokemon_list) + 48
+    img = Image.new("RGB", (width, height), "white")
+
+    # Set the font for the Pokemon names and types
+    name_font = ImageFont.truetype("arial.ttf", 20)
+    type_font = ImageFont.truetype("arial.ttf", 16)
+
+    # Draw the heading
+    draw = ImageDraw.Draw(img)
+    h_text = f"All Pokémon Who Are Weak Against {m_type} Moves ({len(pokemon_list)})"
+    heading_font = ImageFont.truetype("arial.ttf", 24)
+    heading_bbox = draw.textbbox((0, 0), h_text, font=heading_font, align="center")
+    heading_width = heading_bbox[2] - heading_bbox[0]
+    draw.text(((width - heading_width) / 2, 0), h_text, font=heading_font, fill="black", align="center")
+
+    # Add the Pokémon sprites and information to the image
+    x = 0
+    y = 48
+    for pokemon in tqdm(pokemon_list, desc="Creating List Image"):
+        sprite_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{pokemon['id']}.png"
+        sprite_response = requests.get(sprite_url)
+        sprite_data = sprite_response.content
+        sprite_img = Image.open(BytesIO(sprite_data))
+        sprite_img = sprite_img.resize((96, 96))
+        img.paste(sprite_img, (x, y))
+        draw = ImageDraw.Draw(img)
+        types_str = "/".join(pokemon['types'])
+        id_text = f"#{pokemon['id']}"
+        name_text = f"{pokemon['name']} ({types_str})"
+        id_bbox = draw.textbbox((x + 2, y + 36), id_text, font=type_font)
+        id_width = id_bbox[2] - id_bbox[0]
+        draw.text((x + 48 - id_width / 2 + 200, y + 36), id_text, font=type_font, fill="black")
+        name_bbox = draw.textbbox((x + 48, y + 36), name_text, font=name_font)
+        name_width = name_bbox[2] - name_bbox[0]
+        draw.text((x + 48 - name_width / 2 + 480, y + 36), name_text, font=name_font, fill="black")
+        draw.line((0, y, width, y), fill="black")
+        y += 96
+
+    img_file = "pokemon_weak_against_" + m_type.lower() + "_moves1.png"
+    img.save(img_file)
+    os.startfile(img_file)  # Open the image file using the default application
+
+
+def main():
+    # Get the move type from the user
+    move_type = input("Enter a move type: ")
+    create_pokemon_image(get_pokemon_list(move_type), move_type)
+
+
+if __name__ == "__main__":
+    main()
