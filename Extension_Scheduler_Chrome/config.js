d8673af8fb4ca3f08e6f60f73447707e5213f4a8
Skinner <jskinn848@cable.comcast.com>
2023-07-25 11:22:29 -0600

    Added Chrome Extension Scheduler Project to My-Portfolio

---
+++ Extension_Scheduler_Chrome/config.js
@@ -0,0 +1,15 @@
+const config = {
+  defaultOptions: {
+    startHour: 8,
+    startMinute: 0,
+    startAmPm: 'AM',
+    endHour: 4,
+    endMinute: 0,
+    endAmPm: 'PM',
+    checkedExtensions: [],
+    extensionsEnabled: true,
+    activeDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], // Added default active days
+  },
+};
+
+export { config };
